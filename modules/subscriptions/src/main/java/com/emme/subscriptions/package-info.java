f9f1bf8f63be31d396fa2ab8c322d506379836f2
Miky Delzar <miguel.delzar@gmail.com>
2026-09-06 15:45:34 -0600

    style(subscriptions): format module metadata

--- modules/subscriptions/src/main/java/com/emme/subscriptions/package-info.java
+++ modules/subscriptions/src/main/java/com/emme/subscriptions/package-info.java
@@ -1,6 +1,9 @@
 @org.springframework.modulith.ApplicationModule(
     displayName = "Subscriptions",
     allowedDependencies = {
-      "shared :: persistence", "tenancy", "tenancy :: tenant-api", "tenancy :: tenant-events"
+      "shared :: persistence",
+      "tenancy",
+      "tenancy :: tenant-api",
+      "tenancy :: tenant-events"
     })
 package com.emme.subscriptions;

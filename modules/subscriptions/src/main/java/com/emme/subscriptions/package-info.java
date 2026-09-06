0ea568d5f9b4a1b6cff8b3db0549a4478f6ae09b
Miky Delzar <miguel.delzar@gmail.com>
2026-09-06 15:30:43 -0600

    fix(subscriptions): allow tenant API dependency

--- modules/subscriptions/src/main/java/com/emme/subscriptions/package-info.java
+++ modules/subscriptions/src/main/java/com/emme/subscriptions/package-info.java
@@ -1,4 +1,6 @@
 @org.springframework.modulith.ApplicationModule(
     displayName = "Subscriptions",
-    allowedDependencies = {"shared :: persistence", "tenancy", "tenancy :: tenant-events"})
+    allowedDependencies = {
+      "shared :: persistence", "tenancy", "tenancy :: tenant-api", "tenancy :: tenant-events"
+    })
 package com.emme.subscriptions;
